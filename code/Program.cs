e118462da0912e4eacf25cfa675a2fa11bd0b34b
v-valdo <v.sersam@gmail.com>
2024-12-09 09:41:02 +0100

    d2

---
+++ code/Program.cs
@@ -0,0 +1,21 @@
+﻿using problems;
+
+string[] file = File.ReadAllLines("list.txt");
+
+int[] puzzleLeft = new int[file.Length];
+int[] puzzleRight = new int[file.Length];
+
+for (int i = 0; i < file.Length; i++)
+{
+    string[] toPairs = file[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    puzzleLeft[i] = int.Parse(toPairs[0].Trim());
+    puzzleRight[i] = int.Parse(toPairs[1].Trim());
+}
+
+Array.Sort(puzzleLeft);
+Array.Sort(puzzleRight);
+
+//day 1
+WriteLine("Day 1 sln: " + Problems.DayOne(puzzleLeft, puzzleRight));
+//day2
+WriteLine("Day 2 sln: " + Problems.DayTwo(puzzleLeft, puzzleRight));
---
+++ code/problems.cs
@@ -0,0 +1,30 @@
+namespace problems;
+
+public static class Problems
+{
+    public static int DayOne(int[] puzzleLeft, int[] puzzleRight)
+    {
+        //day 1
+        int sumOfDiff = 0;
+
+        for (int i = 0; i < puzzleLeft.Length; i++)
+        {
+            sumOfDiff += puzzleLeft[i] > puzzleRight[i]
+            ? puzzleLeft[i] - puzzleRight[i]
+            : puzzleRight[i] - puzzleLeft[i];
+        }
+
+        return sumOfDiff;
+    }
+
+    public static int DayTwo(int[] puzzleLeft, int[] puzzleRight)
+    {
+        int sum = 0;
+        for (int i = 0; i < puzzleLeft.Length; i++)
+        {
+            sum += puzzleLeft[i] * puzzleRight.Count(pr => pr == puzzleLeft[i]);
+            WriteLine(sum);
+        }
+        return sum;
+    }
+}
